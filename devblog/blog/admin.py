acbb9a2035fdb93392f8a341c47395c3f14207c6
Nick🛰️ <cpcodex23@gmail.com>
2025-08-18 15:20:54 -0400

    update admin

--- devblog/blog/admin.py
+++ devblog/blog/admin.py
@@ -12,5 +12,5 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ['author']
     date_hierarchy = 'publish'
-    ordering = ['status', 'publish']
+    ordering = ['publish', 'status']
     show_facets = admin.ShowFacets.ALWAYS  # type: ignore
