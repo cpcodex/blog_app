058ace91f8219ce95fc761249b806d7ebbb77080
Nick🛰️ <cpcodex23@gmail.com>
2025-08-20 02:23:19 -0400

    format/update; paginator implementation complete

--- devblog/blog/views.py
+++ devblog/blog/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 
@@ -6,9 +6,16 @@ from .models import Post
 def post_list(request):
     post_list = Post.published.all()
     # paginator init
-    paginator = Paginator(post_list, 2)  # pagination with posts per page
+    paginator = Paginator(post_list, 8)  # pagination with posts per page
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    # error handling for page_number not an int
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    # error handling for excess page number in URL
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
 
     return render(request, 'blog/post/list.html', {'posts': posts})
 
